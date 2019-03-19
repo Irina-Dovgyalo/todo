af8b5f0ddcbbf3e1dc3e92983d2e6b7118037e74
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-03-19 16:09:02 +0300

    feat: add functions onLabelClick and onMarkImportant

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
-    render() {
-        const { label, important = false } = this.props;
-        const style = {
-            color: important ? 'tomato' : 'black'
+    constructor(){
+        super();
+        this.state ={
+            done: false,
+            important: false
         };
+    }
+
+    onLabelClick = () => {
+        this.setState((state) => {
+            return {
+                done: !state.done
+            }
+        })
+    }
+
+    onMarkImportant = () => {
+        this.setState((state) => {
+            return {
+                important: !state.important
+            }
+        })
+    }
+
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
+
         return(
-            <span className='todo-list-item'>
-                <span className='todo-list-item-label' style={style}>{label}</span> 
+            <span className={classNames}>
+                <span className='todo-list-item-label' onClick={this.onLabelClick}>{label}</span> 
                 <div className='buttons'>
-                    <button type='button' className='btn btn-outline-success btn-sm'><i className="fas fa-exclamation"></i></button>
+                    <button type='button' className='btn btn-outline-success btn-sm' onClick={this.onMarkImportant}><i className="fas fa-exclamation"></i></button>
                     <button type='button' className='btn btn-outline-danger btn-sm'><i className="far fa-trash-alt"></i></button>
                 </div>            
             </span>
